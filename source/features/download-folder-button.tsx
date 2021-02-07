0143010ccd1e0e77ed1fa69d97ed911f940644ec
Yakov <16872793+yakov116@users.noreply.github.com>
2021-02-07 08:10:31 -0500

    `download-folder-button`

--- source/features/download-folder-button.tsx
+++ source/features/download-folder-button.tsx
@@ -9,28 +9,15 @@ function init(): void {
 	const downloadUrl = new URL('https://download-directory.github.io/');
 	downloadUrl.searchParams.set('url', location.href);
 
-	const folderButtonGroup = select('.file-navigation .BtnGroup.float-right');
-	if (folderButtonGroup) {
-		folderButtonGroup.prepend(
-			<a
-				className="btn btn-sm BtnGroup-item"
-				href={downloadUrl.href}
-			>
-				Download
-			</a>
-		);
-	} else {
-		// "Repository refresh" layout
-		select('.file-navigation > .d-flex:last-child')!.append(
-			<a
-				className="btn ml-2"
-				href={downloadUrl.href}
-			>
-				<DownloadIcon className="mr-1"/>
-				Download
-			</a>
-		);
-	}
+	select('.file-navigation > .d-flex:last-child')!.append(
+		<a
+			className="btn ml-2"
+			href={downloadUrl.href}
+		>
+			<DownloadIcon className="mr-1"/>
+			Download
+		</a>
+	);
 }
 
 void features.add(__filebasename, {
