c527f32cf41fc13fc6fb176fdd1ade813046cd31
Yakov <16872793+yakov116@users.noreply.github.com>
2021-02-07 07:59:17 -0500

    `ci-link`

--- source/features/ci-link.tsx
+++ source/features/ci-link.tsx
@@ -9,10 +9,7 @@ import {buildRepoURL} from '../github-helpers';
 
 // Look for the CI icon in the latest 2 days of commits #2990
 const getIcon = onetime(async () => fetchDom(
-	buildRepoURL('commits'), [
-		'.commit-group:nth-of-type(-n+2) .commit-build-statuses', // Pre "Repository refresh" layout
-		'.TimelineItem--condensed:nth-of-type(-n+2) .commit-build-statuses'
-	].join()
+	buildRepoURL('commits'), '.TimelineItem--condensed:nth-of-type(-n+2) .commit-build-statuses'
 ));
 
 async function init(): Promise<false | void> {
