59b9b4bcf54ed1e191960981f4123cb5ba58b0d8
Yakov <16872793+yakov116@users.noreply.github.com>
2021-02-07 22:58:32 -0500

    `releases-tab`

--- source/features/releases-tab.tsx
+++ source/features/releases-tab.tsx
@@ -15,12 +15,6 @@ import {buildRepoURL, getRepo} from '../github-helpers';
 const getCacheKey = (): string => `releases-count:${getRepo()!.nameWithOwner}`;
 
 function parseCountFromDom(): number {
-	const releasesCountElement = select('.numbers-summary a[href$="/releases"] .num');
-	if (releasesCountElement) {
-		return looseParseInt(releasesCountElement);
-	}
-
-	// In "Repository refresh" layout, look for the releases link in the sidebar
 	const moreReleasesCountElement = select('[href$="/tags"] strong');
 	if (moreReleasesCountElement) {
 		return looseParseInt(moreReleasesCountElement);
@@ -59,81 +53,51 @@ async function init(): Promise<false | void> {
 	}
 
 	// Wait for the tab bar to be loaded
-	await elementReady([
-		'.pagehead', // Pre "Repository refresh" layout
-		'.UnderlineNav-body'
-	].join());
+	await elementReady('.UnderlineNav-body');
 
-	const repoNavigationBar = select('.js-responsive-underlinenav');
-	if (repoNavigationBar) {
-		// "Repository refresh" layout
-		const releasesTab = (
-			<a
-				href={buildRepoURL('releases')}
-				className="js-selected-navigation-item UnderlineNav-item hx_underlinenav-item no-wrap js-responsive-underlinenav-item"
-				data-hotkey="g r"
-				data-selected-links="repo_releases"
-				data-tab-item="rgh-releases-item"
-			>
-				<TagIcon className="UnderlineNav-octicon"/>
-				<span data-content="Releases">Releases</span>
-				{count && <span className="Counter">{count}</span>}
-			</a>
-		);
-
-		select(':scope > ul', repoNavigationBar)!.append(
-			<li className="d-flex">
-				{releasesTab}
-			</li>
-		);
-
-		// This re-triggers the overflow listener forcing it to also hide this tab if necessary #3347
-		repoNavigationBar.replaceWith(repoNavigationBar);
-
-		// Update "selected" tab mark
-		if (pageDetect.isReleasesOrTags()) {
-			const selected = select('.UnderlineNav-item.selected');
-			if (selected) {
-				selected.classList.remove('selected');
-				selected.removeAttribute('aria-current');
-			}
-
-			releasesTab.classList.add('selected');
-			releasesTab.setAttribute('aria-current', 'page');
-		}
-
-		appendBefore(
-			select('.js-responsive-underlinenav .dropdown-menu ul')!,
-			'.dropdown-divider', // Won't exist if `more-dropdown` is disabled
-			createDropdownItem('Releases', buildRepoURL('releases'), {
-				'data-menu-item': 'rgh-releases-item'
-			})
-		);
-
-		return;
-	}
-
+	const repoNavigationBar = select('.js-responsive-underlinenav')!;
 	const releasesTab = (
-		<a href={buildRepoURL('releases')} className="reponav-item" data-hotkey="g r">
-			<TagIcon/>
-			<span> Releases </span>
+		<a
+			href={buildRepoURL('releases')}
+			className="js-selected-navigation-item UnderlineNav-item hx_underlinenav-item no-wrap js-responsive-underlinenav-item"
+			data-hotkey="g r"
+			data-selected-links="repo_releases"
+			data-tab-item="rgh-releases-item"
+		>
+			<TagIcon className="UnderlineNav-octicon"/>
+			<span data-content="Releases">Releases</span>
 			{count && <span className="Counter">{count}</span>}
 		</a>
 	);
 
-	appendBefore(
-		// GHE doesn't have `.reponav > ul`
-		select('.reponav > ul') ?? select('.reponav')!,
-		'.reponav-dropdown, [data-selected-links^="repo_settings"]',
-		releasesTab
+	select(':scope > ul', repoNavigationBar)!.append(
+		<li className="d-flex">
+			{releasesTab}
+		</li>
 	);
+
+	// This re-triggers the overflow listener forcing it to also hide this tab if necessary #3347
+	repoNavigationBar.replaceWith(repoNavigationBar);
 
 	// Update "selected" tab mark
 	if (pageDetect.isReleasesOrTags()) {
-		select('.reponav-item.selected')?.classList.remove('js-selected-navigation-item', 'selected');
-		releasesTab.classList.add('js-selected-navigation-item', 'selected');
-		releasesTab.dataset.selectedLinks = 'repo_releases'; // Required for ajaxLoad
+		const selected = select('.UnderlineNav-item.selected');
+		if (selected) {
+			selected.classList.remove('selected');
+			selected.removeAttribute('aria-current');
+		}
+
+		releasesTab.classList.add('selected');
+		releasesTab.setAttribute('aria-current', 'page');
 	}
+
+	appendBefore(
+		select('.js-responsive-underlinenav .dropdown-menu ul')!,
+		'.dropdown-divider', // Won't exist if `more-dropdown` is disabled
+		createDropdownItem('Releases', buildRepoURL('releases'), {
+			'data-menu-item': 'rgh-releases-item'
+		})
+	);
 }
 
 void features.add(__filebasename, {
